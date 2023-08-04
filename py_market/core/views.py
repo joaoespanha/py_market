e9beb4f879318e12bbb6c417ffc52b67e60fad3f
Joao Espanha <jpbesr@gmail.com>
2023-08-04 14:53:50 -0300

    implementing dash board of newest items and categories in the  home page

--- py_market/core/views.py
+++ py_market/core/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-
+from item.models import Category, Item
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def index(request):
-    return render(request, "core/index.html", {})
+    items = Item.objects.all()[0:6]
+    categories = Category.objects.all()[0:3]
+
+    return render(request, "core/index.html", {"items": list(items), "categories":categories})
 
 
 def contact(request):
+    
     return render(request, "core/contact.html", {})
--- py_market/py_market/urls.py
+++ py_market/py_market/urls.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 from core.views import index, contact
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
     path("contact/", contact, name="contact"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
